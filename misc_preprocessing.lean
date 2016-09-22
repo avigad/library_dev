66dde5595a6cf4d0486c81f84de5e1db6b47025d
Gabriel Ebner <gebner@gebner.org>
2016-09-22 10:26:42 -0400

    Fix tautology removal.

--- misc_preprocessing.lean
+++ misc_preprocessing.lean
@@ -1,11 +1,19 @@
 import clause prover_state
-open expr list
+open expr list monad
 
-meta_definition is_taut (c : cls) : bool := do
-list.bor (do
-  l1 ← cls.get_lits c, guard $ cls.lit.is_neg l1,
-  l2 ← cls.get_lits c, guard $ cls.lit.is_pos l2,
-  [alpha_eqv (cls.lit.formula l1) (cls.lit.formula l2)])
+meta_definition is_taut (c : cls) : tactic bool := do
+qf ← cls.open_constn c c↣num_quants,
+return $ list.bor (do
+  l1 ← cls.get_lits qf.1, guard $ cls.lit.is_neg l1,
+  l2 ← cls.get_lits qf.1, guard $ cls.lit.is_pos l2,
+  [decidable.to_bool (cls.lit.formula l1 = cls.lit.formula l2)])
+
+open tactic
+example (i : Type) (p : i → i → Type) (c : i) (h : ∀ (x : i), p x c → p x c) : true := by do
+h ← get_local `h, hcls ← monad.liftM (cls.mk 1 2 tt h) (infer_type h),
+taut ← is_taut hcls,
+when (¬taut) failed,
+to_expr `(trivial) >>= apply
 
 meta_definition tautology_removal_pre : resolution_prover unit :=
-preprocessing_rule $ λnew, return (filter (λc, bool.bnot (is_taut c)) new)
+preprocessing_rule $ λnew, resolution_prover_of_tactic (filterM (λc, liftM bool.bnot (is_taut c)) new)
