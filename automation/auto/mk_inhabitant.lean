9da8721b6a90b629fdf10419ec75db1ae6e30ac7
Jeremy Avigad <avigad@cmu.edu>
2016-10-04 15:45:44 -0400

    fix(auto.lean,*): move to automation subfolder, and fix mk_inhabitant

--- automation/auto/mk_inhabitant.lean
+++ automation/auto/mk_inhabitant.lean
@@ -2,7 +2,7 @@ open tactic
 
 /- mk_inthabitant_using A t assumes A is an expression denoting a type. It creates
    a goal with type A, applies t, and returns the expression that results. -/
-meta_definition mk_inhabitant_using (A : expr) (t : tactic unit) : tactic expr :=
+meta def mk_inhabitant_using (A : expr) (t : tactic unit) : tactic expr :=
 do m ← mk_meta_var A,
    gs ← get_goals,
    set_goals [m],
@@ -11,7 +11,7 @@ do m ← mk_meta_var A,
    set_goals gs,
    return r
 
-meta_definition my_tac : tactic expr :=
+meta def my_tac : tactic expr :=
 do A ← to_expr `(true ∧ true),
    mk_inhabitant_using A (trace_state >> repeat constructor)
 
